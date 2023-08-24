9937712e8467e56539fc4f75b9b2609f8941e422
khaled <khaled.gaguiga@holbertonschool.com>
2023-08-24 12:06:13 +0100

    pycodestyles

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
+'''
+Modulos that contains a function that
+returns two placeholders, x and y
+'''
+import tensorflow.compat.v1 as tf
 
-import tensorflow as tf
 
 def create_placeholders(nx, classes):
-    """create placeholders for a neural network classifier"""
-    x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
-    y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
+    '''
+    Function that returns two place holders, x an y
+
+    Parameters
+    ----------
+    nx : int
+        Number of feature columns in our data.
+    classes : int
+        Number of classes in our classifier.
+
+    Returns
+    -------
+    Placeholders x and y.
+
+    '''
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
     return x, y
