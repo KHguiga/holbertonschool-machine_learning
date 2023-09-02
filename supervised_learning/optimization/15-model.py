10fc811168be213ce9af89066bb578a50596ec17
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-02 10:53:03 +0100

    task 15 ray

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -93,9 +93,9 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
     global_step = tf.Variable(0, trainable=False)
 
     decay_steps = m // batch_size * epochs
-    # if m % batch_size:
-    #     decay_steps += 1
-
+    if m % batch_size:
+        decay_steps += 1
+    decay_steps *= epochs
     alpha = learning_rate_decay(alpha, decay_rate, global_step, decay_steps)
 
     train_op = create_Adam_op(loss, alpha, beta1, beta2, epsilon, global_step)
