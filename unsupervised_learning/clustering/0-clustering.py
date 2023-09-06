bcdbb7a8205ad9b7d35b8f141e38dd811e819319
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-06 19:54:52 +0100

    clustering

---
+++ unsupervised_learning/clustering/0-clustering.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def squared_dists(A, B):
+    M = A.shape[0]
+    N = B.shape[0]
+    A_dots = (A * A).sum(axis = 1).reshape((M, 1)) * np.ones(shape=(1, N))
+    B_dots = (B * B).sum(axis = 1) * np.ones(shape = (M, 1))
+    return  A_dots + B_dots - 2 * A.dot(B.T)
