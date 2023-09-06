c78513243f805795d0a6e88099be82b80db03727
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-06 20:04:32 +0100

    clustering task 1

---
+++ unsupervised_learning/clustering/1-clustering.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def squared_dists( A, B ):
+    M = A.shape[0]
+    N = B.shape[0]
+    A_dots = (A*A).sum(axis=1).reshape((M,1))*np.ones(shape=(1,N))
+    B_dots = (B*B).sum(axis=1)*np.ones(shape=(M,1))
+    return  A_dots + B_dots -2*A.dot(B.T)
+
+def associated_clustering( points, centroids ):
+    return squared_dists(points, centroids).argmin(axis=1)
