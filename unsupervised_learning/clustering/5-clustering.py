523c4fcea55b98fb70de9f4f1fbd6e757beb8d95
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-06 22:03:32 +0100

    clustering task 5

--- unsupervised_learning/clustering/5-clustering.py
+++ unsupervised_learning/clustering/5-clustering.py
@@ -21,14 +21,17 @@ def K_means( points , k, n_iter=100, n_init=100, seed=0):
     inert= 1.
     np.random.seed(seed)
     inertias=[]
+    iterations_needed = 0
     for i in range(n_init):
         gen_0 = random_init_centroids(points,k)
         d = K_means_with_given_gen_0(points, gen_0, n_iter)
         inertias.append(d['inertia'])
+        iterations_needed += d['iterations needed']
         if d['inertia'] < inert or i == 0 :
             inert = d['inertia']
             best_k = d
     inertias = np.array(inertias)
     best_k['mean inertia'] = np.mean(inertias)
     best_k['standard deviation inertias'] = np.std(inertias)
+    best_k['mean iterations needed'] = np.mean(inertias)
     return best_k
