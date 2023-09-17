ec10b3ad6f853adbf174293b014cf20c4d448c6b
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-17 19:10:25 +0100

    cl 8

--- unsupervised_learning/clustering/8-clustering.py
+++ unsupervised_learning/clustering/8-clustering.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import networkx as nx
 
+def calculate_distances(points):
+    # Number of data points
+    vectors = points[None,:,:]-points[:,None,:]
+    return np.power(vectors,2).sum(axis=2)
+
+def dist_based_clust(points, rho):
+    # Calculate pairwise distances
+    distances = calculate_distances(points)
+    
+    # Create a graph where edges connect points within the rho-neighborhood
+    m = points.shape[0]
+    G = nx.Graph()
+    
+    for i in range(m):
+        for j in range(i, m):
+            if distances[i,j] < rho**2:
+                G.add_edge(i, j)
+    
+    # Find connected components in the graph
+    components = list(nx.connected_components(G))
+    s = len(components)
+    clustering = [0]*m
+    for cluster_idx, component in enumerate(components):
+        for point_idx in component:
+            clustering[point_idx] = cluster_idx
+                
+    # Create the clustering array
+    return np.array(clustering)
