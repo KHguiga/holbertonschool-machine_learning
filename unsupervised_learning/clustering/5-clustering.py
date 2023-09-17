6a9ec12dae5f08ffc55ef8cd63c516b168684a3d
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-17 18:52:27 +0100

    cl 5

--- unsupervised_learning/clustering/5-clustering.py
+++ unsupervised_learning/clustering/5-clustering.py
@@ -3,6 +3,20 @@
 import numpy as np
 K_means_with_given_gen_0 = __import__('4-clustering').K_means_with_given_gen_0
 
+def random_init_centroids(points,K):
+    
+    n = points.shape[1]
+
+    mi =np.min(points, axis=0)
+
+    Ma = np.max(points, axis=0)
+
+    P = np.random.rand(K*n).reshape((K,n))
+
+    P *= (Ma - mi)[None, :]
+    P += mi
+    return P
+
 def K_means( points , k, n_iter=100, n_init=100, seed=0):
     inert= np.inf
     np.random.seed(seed)
