a8d1e2d6c9ece77ba654b09c42c82456232ca36a
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-17 18:36:50 +0100

    cl 3

---
+++ unsupervised_learning/clustering/3-clustering.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+squared_dists = __import__('0-clustering').squared_dists
+
+
+def inertia(points, centroids, clustering):
+    k = centroids.shape[0]
+    inertia = 0
+    for i in range(k):
+        cluster_points = points[clustering == i]
+        cluster_variance = np.sum(squared_dists( cluster_points, np.array([centroids[i]]))) 
+        inertia += cluster_variance
+    return inertia / points.shape[0]
