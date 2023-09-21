d548eba2963f8b8b90f33b7bc9a62ed8b526da6e
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-21 10:59:04 +0100

    update task 9

--- unsupervised_learning/clustering/9-clustering.py
+++ unsupervised_learning/clustering/9-clustering.py
@@ -8,6 +8,7 @@ def dist_based_best_rho(points, rho_min, rho_max, n_sample=100):
     max_count = 0
     count = 0
     start_idx = 0
+    max_start_idx = 0
     ks = np.arange(n_sample+1)
     rhos = rho_min + ks * (rho_max - rho_min) / n_sample
     for rho_k in rhos:
@@ -16,10 +17,15 @@ def dist_based_best_rho(points, rho_min, rho_max, n_sample=100):
     for i in range(1, len(clusters)):
         if clusters[i] == 1: # si nombre de clusters est 1 on sort de la boucle
             break
-        if clusters[i] == clusters[i-1]:
-            count += 1
-            max_count = max(max_count, count)
         else:
-            count = 0
-            start_idx = i
-    return (rhos[start_idx] + rhos[start_idx + max_count]) / 2
+            if clusters[i] == clusters[i-1]:
+                count += 1
+                if count > max_count:
+                    max_start_idx = start_idx
+                    max_count = count
+#                 max_count = max(max_count, count)
+            else:
+                
+                count = 0
+                start_idx = i
+    return (rhos[max_start_idx] + rhos[max_start_idx + max_count]) / 2
