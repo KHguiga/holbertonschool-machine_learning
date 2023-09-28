de34d6ffb5fa70476e6163c215eb66fcb336d79f
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-28 13:44:05 +0100

    deep cnn t7

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -6,15 +6,13 @@ import tensorflow.keras as K
 
 def transition_layer(X, nb_filters, compression):
 
-    nb_filters = int(nb_filters * compression)
+    init = K.initializers.he_normal
+    compressed_filters = int(nb_filters * compression)
 
-    # 1x1 conv
-    X = K.layers.BatchNormalization(axis=3)(X)
+    X = K.layers.BatchNormalization()(X)
     X = K.layers.ReLU()(X)
-    X = K.layers.Conv2D(nb_filters, (1, 1), padding='same',
-                        kernel_initializer=K.initializers.he_normal())(X)
+    X = K.layers.Conv2D(compressed_filters, 1,
+                        padding="same", kernel_initializer=init)(X)
+    X = K.layers.AveragePooling2D(2, 2)(X)
 
-    # average pool
-    X = K.layers.AveragePooling2D((2, 2), strides=(2, 2), padding='same')(X)
-
-    return X, nb_filters
+    return X, compressed_filters
