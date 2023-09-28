0c9c05406693bec97bf093aee243226d209184a3
khaled <khaled.gaguiga@holbertonschool.com>
2023-09-28 09:50:45 +0100

    deep cnn 7

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -13,7 +13,8 @@ def identity_block(A_prev, filters):
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
-    X = K.layers.Conv2D(F3, (3, 3), padding='same', kernel_initializer=he_norm)(X)
+    X = K.layers.Conv2D(F3, (3, 3), padding='same', 
+                        kernel_initializer=he_norm)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
@@ -22,5 +23,5 @@ def identity_block(A_prev, filters):
 
     X = K.layers.Add()([X, A_prev])
     X = K.layers.Activation('relu')(X)
-    
+
     return X
