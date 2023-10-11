05c80f6dfa56122206d6e0a395269b12c0ed9bff
khaled <khaled.gaguiga@holbertonschool.com>
2023-10-11 16:58:33 +0100

    od thameur

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -22,8 +22,8 @@ class Yolo:
         if not os.path.exists(classes_path):
             raise FileNotFoundError("Wrong classes file path")
         self.model = K.models.load_model(model_path)
-        with open(classes_path, 'r') as f:
-            self.class_names = [line[:-1] for line in f]
+        with open(classes_path) as f:
+            self.class_names = [class_name.strip() for class_name in f.readlines()]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
