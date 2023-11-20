c98bc46af5a9948cb936160c158e651891166042
khaled <khaled.gaguiga@holbertonschool.com>
2023-11-20 09:06:39 +0100

    add import in comment

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -3,6 +3,17 @@
 import tensorflow.keras as K
 
 def inception_block(A_prev, filters):
+    """Performs K-means on a dataset:
+
+    X is a numpy.ndarray of shape (n, d) containing the dataset
+    k is the number of clusters
+    The only import you are allowed to use is import sklearn.cluster
+    Returns: C, clss
+    C is a numpy.ndarray of shape (k, d) containing the centroid means for
+    each cluster
+    clss is a numpy.ndarray of shape (n,) containing the index of the cluster
+    in C that each data point belongs to
+    """
 
     F1, F3R, F3, F5R, F5, FPP = filters
 
