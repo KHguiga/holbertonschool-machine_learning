2f5e79df47a5d2a29ed6a9c857c18a5a63a822a5
khaled <khaled.gaguiga@holbertonschool.com>
2024-02-28 17:08:13 +0100

    img size vs mode task 1

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -1,135 +1,91 @@
 #!/usr/bin/env python3
-"""
-    Initialize Yolo
-"""
-import tensorflow as tf
+"""this module contains the class Yolo"""
+import tensorflow.keras as K
 import numpy as np
+import os
+
+
+def sigmoid(x):
+    """sigmoid function"""
+    return 1 / (1 + np.exp(-x))
 
 
 class Yolo:
-    """
-        Class Yolo uses the Yolo v3 algorithm to perform object detection
-    """
+    """Yolo class"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Constructor method
         """
-            Class constructor of Yolo class
+        if not os.path.exists(model_path):
+            raise FileNotFoundError("Wrong model file path")
 
-            :param model_path: path where Darknet Keras model is stored
-            :param classes_path:path where list of class names,
-                in order of index
-            :param class_t: float, box score threshold
-                for initial filtering step
-            :param nms_t: float, IOU threshold for non-max suppression
-            :param anchors: ndarray, shape(outputs, anchor_boxes, 2)
-                all anchor boxes
-                outputs: number of outputs (prediction) made by Darknet model
-                anchor_boxes: number of anchor boxes used for each prediction
-                2: [anchor_box_width, anchor_box_height]
-
-        """
-        self.model = tf.keras.models.load_model(model_path)
-        self.class_names = []
+        if not os.path.exists(classes_path):
+            raise FileNotFoundError("Wrong classes file path")
+        self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
-            for line in f:
-                line = line.strip()
-                self.class_names.append(line)
+            self.class_names = [line[:-1] for line in f]
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
 
     def process_outputs(self, outputs, image_size):
-        """
-            Function to process outputs
-
-        :param outputs: list of ndarray, predictions from a single image
-                each output,
-                shape(grid_height, grid_width, anchor_boxes, 4+1+classes)
-                grid_height, grid_width: height and width of grid
-                 used for the output
-                anchor_boxes: number of anchor boxes used
-                4 => (t_x, t_y, t_w, t_h)
-                1 => box_confidence
-                classes => classes probabilities for all classes
-        :param image_size: ndarray,
-               image's original size [image_height, image_width]
-
-        :return: tuple (boxes, box_confidences, box_class_probs):
-                boxes: list of ndarrays,
-                       shape(grid_height, grid_width, anchor_boxes, 4)
-                        processed boundary boxes for each output
-                        4 => (x1,y1, x2, y2)
-                boxe_confidences: list ndarray,
-                    shape(grid_height, grid_width, anchor_boxes, 1)
-                    boxe confidences for each output
-                box_class_probs: list ndarray,
-                    shape(grid_height, grid_width, anchor_boxes, classes)
-                    box's class probabilities for each output
-        """
-        # extract image size
-        image_height, image_height = image_size
-
+        """Process and normalize the output of the YoloV3 model"""
         boxes = []
         box_confidences = []
         box_class_probs = []
-
-        # process for each output
-        for idx, output in enumerate(outputs):
-
-            # extract height, width, number of anchor box for current output
-            grid_height, grid_width, nbr_anchor, _ = output.shape
-
-            # extract coordinate of output NN
+        img_h, img_w = image_size
+        i = 0
+        for output in outputs:
+            grid_h, grid_w, nb_box, _ = output.shape
+            box_conf = np.expand_dims(sigmoid(output[:, :, :, 4]), axis=-1)
+            box_prob = sigmoid(output[:, :, :, 5:])
+            box_confidences.append(box_conf)
+            box_class_probs.append(box_prob)
+            # t_x, t_y : x and y coordinates of the center pt of the anchor box
+            # t_w, t_h : width and height of the anchor box
             t_x = output[:, :, :, 0]
             t_y = output[:, :, :, 1]
             t_w = output[:, :, :, 2]
             t_h = output[:, :, :, 3]
+            # c_x, c_y : is going to represent the grid of indexes
+            c_x = np.arange(grid_w)
+            c_x = np.tile(c_x, grid_h)
+            c_x = c_x.reshape(grid_h, grid_w, 1)
 
-            # grid coordinate
-            grid_x, grid_y = np.meshgrid(np.arange(grid_width),
-                                         np.arange(grid_height))
+            c_y = np.arange(grid_h)
+            c_y = np.tile(c_y, grid_w)
+            c_y = c_y.reshape(1, grid_h, grid_w).T
 
-            # Repeat grid coordinate for each anchor box
-            grid_x = np.expand_dims(grid_x, axis=-1)
-            grid_y = np.expand_dims(grid_y, axis=-1)
-            print("c_x",grid_x.shape, "c_y",grid_y.shape)
-            # extract anchor_box_width, anchor_box_height
-            p_w = self.anchors[idx, :, 0]
-            p_h = self.anchors[idx, :, 1]
+            # p_w, p_h : anchors dimensions in the c
 
-            # size image
-            image_width, image_height = image_size
+            p_w = self.anchors[i, :, 0]
+            p_h = self.anchors[i, :, 1]
 
-            # sigmoid : grid scale (value between 0 and 1)
-            # + c_x or c_y : coordinate of cells in the grid
-            b_x = (1.0 / (1.0 + np.exp(-t_x)) + grid_x) / grid_width
-            b_y = (1.0 / (1.0 + np.exp(-t_y)) + grid_y) / grid_height
-            # exp for predicted height and width
-            b_w = p_w * np.exp(t_w)
-            b_w /= self.model.input.shape[1].value
-            b_h = p_h * np.exp(t_h)
-            b_h /= self.model.input.shape[2].value
+            # yolo formula (get the coordinates in the prediction box)
+            b_x = (sigmoid(t_x) + c_x)
+            b_y = (sigmoid(t_y) + c_y)
+            b_w = (np.exp(t_w) * p_w)
+            b_h = (np.exp(t_h) * p_h)
+            # normalize to the input size
+            b_x = b_x / grid_w
+            b_y = b_y / grid_h
+            b_w = b_w / self.model.input.shape[1].value
+            b_h = b_h / self.model.input.shape[2].value
+            # scale to the image size (in pixels)
+            # top left corner
+            x1 = (b_x - b_w / 2) * img_w
+            y1 = (b_y - b_h / 2) * img_h
+            # bottom right corner
+            x2 = (b_x + b_w / 2) * img_w
+            y2 = (b_y + b_h / 2) * img_h
+            print("x2",x2, "y2",y2)
 
-            # conv in pixel : absolute coordinate
-            x1 = (b_x - b_w / 2) * image_width
-            y1 = (b_y - b_h / 2) * image_height
-            x2 = x1 + b_w * image_width
-            y2 = y1 + b_h * image_height
-
-            # Update box array with box coordinates and dimensions
-            box = np.zeros((grid_height, grid_width, nbr_anchor, 4))
+            # create the current box
+            box = np.zeros((grid_h, grid_w, nb_box, 4))
             box[:, :, :, 0] = x1
             box[:, :, :, 1] = y1
             box[:, :, :, 2] = x2
             box[:, :, :, 3] = y2
             boxes.append(box)
-
-            confidences = output[:, :, :, 4:5]
-            sigmoid_confidence = 1 / (1 + np.exp(-confidences))
-            class_probs = output[:, :, :, 5:]
-            sigmoid_class_probs = 1 / (1 + np.exp(-class_probs))
-
-            box_confidences.append(sigmoid_confidence)
-            box_class_probs.append(sigmoid_class_probs)
-
+            i += 1
         return boxes, box_confidences, box_class_probs
