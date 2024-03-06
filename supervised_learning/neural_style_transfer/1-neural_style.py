05a2612533649e0807bff2eaabe02af1b9f9ddaa
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-06 14:09:07 +0100

    nst t1

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -31,8 +31,7 @@ class NST:
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
-        self.model = self.load_model()
-
+        self.load_model()
     @staticmethod
     def scale_image(image):
         if not isinstance(image, np.ndarray
@@ -40,7 +39,7 @@ class NST:
             raise TypeError(
                 "image must be a numpy.ndarray with shape (h, w, 3)")
 
-        h, w, c = image.shape
+        h, w, _ = image.shape
         if h > w:
             h_new = 512
             w_new = w * h_new // h
@@ -57,20 +56,20 @@ class NST:
         return scaled_image
 
     def load_model(self):
-        # Load our model. We load pretrained VGG, trained on imagenet data
         vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
-        vgg.trainable = False
-
-        # Convert MaxPooling2D to AveragePooling2D for style layers
-        for layer in vgg.layers:
-            if 'block' in layer.name and 'pool' in layer.name:
-                layer.__class__ = tf.keras.layers.AveragePooling2D
-
-        # Get output layers corresponding to style and content layers
-        model_outputs = [vgg.get_layer(name).output for name in self.style_layers]
-        model_outputs.append(vgg.get_layer(self.content_layer).output)
-
-        # Build model
+        x = vgg.input
+        model_outputs = []
+        content_output = None
+        for layer in vgg.layers[1:]:
+            if "pool" in layer.name:
+                x = tf.keras.layers.AveragePooling2D(pool_size=layer.pool_size, strides=layer.strides, name=layer.name)(x)
+            else:
+                x = layer(x)
+                if layer.name in self.style_layers:
+                    model_outputs.append(x)
+                if layer.name == self.content_layer:
+                    content_output = x
+                layer.trainable = False
+        model_outputs.append(content_output)
         model = tf.keras.models.Model(vgg.input, model_outputs)
         self.model = model
-        return model
