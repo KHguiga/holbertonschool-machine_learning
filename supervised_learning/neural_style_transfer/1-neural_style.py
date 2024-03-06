a543299352864ad5a9f98acbd885498573910522
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-06 03:32:39 +0100

    nst t1

---
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""Neural Style Transfer Module"""
+import numpy as np
+import tensorflow as tf
+
+
+class NST:
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
+    content_layer = 'block5_conv2'
+
+    def __init__(self, style_image, content_image, alpha=1e4, beta=1):
+        if not isinstance(style_image, np.ndarray
+                          ) or len(style_image.shape
+                                   ) != 3 or style_image.shape[2] != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(content_image, np.ndarray
+                          ) or len(content_image.shape
+                                   ) != 3 or content_image.shape[2] != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
+        if not isinstance(alpha, float) and not isinstance(alpha, int
+                                                           ) or alpha < 0:
+            raise TypeError("alpha must be a non-negative number")
+        if not isinstance(beta, float) and not isinstance(beta, int
+                                                          ) or beta < 0:
+            raise TypeError("beta must be a non-negative number")
+
+        self.style_image = self.scale_image(style_image)
+        self.content_image = self.scale_image(content_image)
+        self.alpha = alpha
+        self.beta = beta
+        self.model = self.load_model()
+    @staticmethod
+    def scale_image(image):
+        if not isinstance(image, np.ndarray
+                          ) or len(image.shape) != 3 or image.shape[2] != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
+
+        h, w, _ = image.shape
+        if h > w:
+            h_new = 512
+            w_new = w * h_new // h
+        else:
+            w_new = 512
+            h_new = h * w_new // w
+
+        scaled_image = tf.image.resize(image, tf.constant([h_new, w_new],
+                                                          dtype=tf.int32),
+                                       tf.image.ResizeMethod.BICUBIC)
+        scaled_image = tf.reshape(scaled_image, (1, h_new, w_new, 3))
+        scaled_image = tf.clip_by_value(scaled_image / 255, 0.0, 1.0)
+
+        return scaled_image
+
+    def load_model(self):
+        # Load our model. We load pretrained VGG, trained on imagenet data
+        vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+        vgg.trainable = False
+        # Get output layers corresponding to style and content layers 
+        model_outputs = [vgg.get_layer(name).output for name in self.style_layers]
+        model_outputs.append(vgg.get_layer(self.content_layer).output)
+        # Build model
+        model = tf.keras.models.Model(vgg.input, model_outputs)
+        self.model = model
+        return model
