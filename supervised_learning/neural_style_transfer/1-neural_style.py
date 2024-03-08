60a0221b5433c915ce194c594db33b856052cc43
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-08 14:06:08 +0100

    nst t1 new load model func

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -56,20 +56,18 @@ class NST:
         return scaled_image
 
     def load_model(self):
+        # Load our model. We load pretrained VGG, trained on imagenet data
         vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
-        x = vgg.input
-        model_outputs = []
-        content_output = None
-        for layer in vgg.layers[1:]:
-            if "pool" in layer.name:
-                x = tf.keras.layers.AveragePooling2D(pool_size=layer.pool_size, strides=layer.strides, name=layer.name)(x)
-            else:
-                x = layer(x)
-                if layer.name in self.style_layers:
-                    model_outputs.append(x)
-                if layer.name == self.content_layer:
-                    content_output = x
-                layer.trainable = False
-        model_outputs.append(content_output)
+        vgg.trainable = False
+        
+        # Convert MaxPooling2D to AveragePooling2D for style layers
+        for layer in vgg.layers:
+            if 'block' in layer.name and 'pool' in layer.name:
+                layer.__class__ = tf.keras.layers.AveragePooling2D
+        # Get output layers corresponding to style and content layers 
+        model_outputs = [vgg.get_layer(name).output for name in self.style_layers]
+        model_outputs.append(vgg.get_layer(self.content_layer).output)
+        # Build model
         model = tf.keras.models.Model(vgg.input, model_outputs)
         self.model = model
+        return model
