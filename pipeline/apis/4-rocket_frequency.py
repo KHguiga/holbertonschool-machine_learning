b095ec7128ff1cc05a49108d99b34d9eda52564c
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-27 18:02:44 +0100

    t4 apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -8,7 +8,4 @@ import requests
 if __name__ == '__main__':
 
     url = 'https://api.spacexdata.com/v3/launches'
-    try:
-        res = requests.get(url).json()
-    except:
-        print("ok")
+    print("ok")
