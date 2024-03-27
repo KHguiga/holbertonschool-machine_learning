e14281db06dfb5432cba66ac18354f1dff1b031d
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-27 18:15:07 +0100

    t4 apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -20,4 +20,16 @@ if __name__ == '__main__':
 
     for launch in launches:
         rocket_id = launch.get('rocket')
-        print(rocket_id)
+        rocket = requests.get(BASE_ROCKET_URL + rocket_id).json()
+        rocket_name = rocket.get('name')
+
+        if rocket_name in rockets.keys():
+            rockets[rocket_name] += 1
+        else:
+            rockets[rocket_name] = 1
+
+    sort = sorted(rockets.items(), key=lambda x: x[0])
+    sort = sorted(sort, key=lambda x: x[1], reverse=True)
+
+    for i in sort:
+        print("{}: {}".format(i[0], i[1]))
