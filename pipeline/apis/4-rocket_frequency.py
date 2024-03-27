69ef3fac4acf611151d1e35def337aa34291656e
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-27 18:07:40 +0100

    t4 apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python3
+
+
+"""
+Useless
+"""
+
+
 import requests
 
 
+BASE_LAUNCH_URL = 'https://api.spacexdata.com/v3/launches'
+BASE_ROCKET_URL = "https://api.spacexdata.com/v4/rockets/"
+
 if __name__ == '__main__':
-    print("ok")
+
+    rockets = {}
+
+    launches = requests.get(BASE_LAUNCH_URL).json()
+
+    for launch in launches:
+        rocket_id = launch.get('rocket')
+        print(rocket_id)
