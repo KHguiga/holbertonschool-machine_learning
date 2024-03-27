3567398725b17e4190a8d39e1ea666731608365a
khaled <khaled.gaguiga@holbertonschool.com>
2024-03-27 18:04:23 +0100

    t4 apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-"""
-script that displays the number of launches per rocket,
-using the (unofficial) SpaceX API
-"""
 import requests
 
-if __name__ == '__main__':
 
-    url = 'https://api.spacexdata.com/v3/launches'
+if __name__ == '__main__':
     print("ok")
