2a9fe86a0161b8823b6bfdee7acd1a130eb17455
khaled <khaled.gaguiga@holbertonschool.com>
2024-05-06 22:11:17 +0100

    t2 100

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
-'''
-Modulus that contains a functiont that contains the
-forward propagation graph for a neural network
-'''
+"""
+    Function Forward propagation
+"""
+
 import tensorflow.compat.v1 as tf
 create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
-    '''
-    functiont that contains the forward propagation
-    graph for a neural network
+    """
+        Method to creates the forward propagation graph for NN
 
-    Parameters
-    ----------
-    x : Placeholder
-        Input data.
-    layer_sizes : list
-        DESCRIPTION. Contains the number of nodes in each layer.
-    activations : list
-        DESCRIPTION. Contains the activation functions for each layer.
+        :param x: placeholder for input data
+        :param layer_sizes: list number of nodes in each layer of NN
+        :param activations: list activation function for each layer
 
-    Returns: The prediction of network tensor form
-    '''
+        :return: the prediction of NN in tensor form
+    """
     for i in range(len(layer_sizes)):
-        if i == 0:
-            estimation = create_layer(x, layer_sizes[i], activations[i])
-        else:
-            estimation = create_layer(estimation, layer_sizes[i],
-                                      activations[i])
-    return estimation
+        new_layer = create_layer(x, layer_sizes[i], activations[i])
+        x = new_layer
+
+    return new_layer
