2d6bdf1eb0853c546dadfe9c2adac5ad4db7e40b
khaled <khaled.gaguiga@holbertonschool.com>
2024-05-11 21:42:16 +0100

    rmsprop scratch

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    update_variables_RMSProp
+"""
+
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    s = beta2 * s + (1 - beta2) * (grad ** 2)
+    var -= alpha * grad / (np.sqrt(s) + epsilon)
+    return var, s
