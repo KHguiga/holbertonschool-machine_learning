d85fc085808f1df20f45520bdd1113e0d8ff7216
khaled <khaled.gaguiga@holbertonschool.com>
2024-05-11 19:31:09 +0100

    moving average

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+   Mini-batch
+"""
+def moving_average(data, beta):
+    avg = []
+    prev = 0
+    for i, d in enumerate(data):
+        #weighted sum
+        prev = beta * prev + (1 - beta) * d
+        #correction
+        correction = prev / (1 - (beta ** (i + 1)))
+        avg.append(correction)
+    return avg
