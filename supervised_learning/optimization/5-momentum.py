0bf8f0765c1aece4790de74e6aba2e00c7665776
khaled <khaled.gaguiga@holbertonschool.com>
2024-05-11 19:38:50 +0100

    momentum scratch

---
+++ supervised_learning/optimization/5-momentum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+   momentum scratch
+"""
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    v = beta1 * v + (1 - beta1) * grad
+    var -= alpha * v
+    return var, v
