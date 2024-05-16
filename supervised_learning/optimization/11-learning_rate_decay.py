6a8866176ec843ce469699a6d8bed4e318ea02dc
khaled <khaled.gaguiga@holbertonschool.com>
2024-05-16 15:14:01 +0100

    t11 jesus

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
+"""This module contains the function for updating a variable
+using the Adam
+optimization algorithm
 """
-    learning_rate_decay
-"""
-
 import numpy as np
 
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
-    return alpha / (1 + decay_rate * (global_step // decay_step))
+    """
+    Update the learning rate using inverse time decay in numpy.
+
+    Parameters:
+    alpha (float): The original learning rate.
+    decay_rate (float): The weight used to determine the rate at
+    which alpha will decay.
+    global_step (int): The number of passes of gradient descent that
+    have elapsed.
+    decay_step (int): The number of passes of gradient descent that should
+    ccur before alpha is decayed further.
+
+    Returns:
+    float: The updated value for alpha.
+    """
+
+    # Calculate the decay factor
+    decay_factor = 1 / (1 + decay_rate * (global_step // decay_step))
+
+    # Update the learning rate
+    alpha_updated = alpha * decay_factor
+
+    # Return the updated learning rate
+    return alpha_updated
