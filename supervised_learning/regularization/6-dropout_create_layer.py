a7a516cf3b142b0b5accbc7ce9690af388912b1a
khaled <khaled.gaguiga@holbertonschool.com>
2024-05-17 18:51:21 +0100

    reg t6

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -5,5 +5,5 @@ import tensorflow.compat.v1 as tf
 def dropout_create_layer(prev, n, activation, keep_prob):
     layer = tf.layers.Dense(n, activation=activation, kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg")), name='layer')
     A = layer(prev)
-    drop = tf.layers.Dropout(1 - keep_prob)
+    drop = tf.layers.Dropout(keep_prob)
     return drop(A)
