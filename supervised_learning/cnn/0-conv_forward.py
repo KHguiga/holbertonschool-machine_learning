79a0a6ad7876e295ce6b312005ce6a5f89252e93
khaled <khaled.gaguiga@holbertonschool.com>
2024-06-03 16:44:22 +0100

    cedric

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,34 +1,67 @@
 #!/usr/bin/env python3
-
+"""
+project CNN
+by Ced
+"""
 import numpy as np
 
-def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    m, h, w, _ = A_prev.shape
-    fh, fw, _, c = W.shape
-    sh, sw = stride
 
+def relu(Z):
+    return np.maximum(Z, 0)
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """
+    to be continued
+    """
+    # A_prev is the input of the convolution layer
+    m = A_prev.shape[0]
+    h_prev = A_prev.shape[1]
+    w_prev = A_prev.shape[2]
+    c_prev = A_prev.shape[3]
+    # print("Al-1", m,h_prev,w_prev,c_prev)
+    
+    # W containing the kernels for the convolution
+    kh = W.shape[0]
+    kw = W.shape[1]
+    c_prev = W.shape[2]
+    c_new = W.shape[3]
+    # print("Weights", kh, kw, c_prev, c_new)
+
+    # define stride    
+    sh = stride[0]
+    sw = stride[1]
+
+    if padding == 'valid':
+        ph = 0
+        pw = 0
     if padding == 'same':
-        nh, nw = h, w
-        ph  = int((fh + (sh * (h - 1)) - h) / 2)
-        pw  = int((fw + (sw * (w - 1)) - w) / 2)
-        A_prev = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
-    elif padding == 'valid':
-        nh = ((h - fh) // sh) + 1
-        nw = ((w - fw) // sw) + 1
-    else:
-        ph, pw = padding
-        nh = ((h - fh + (2 * ph)) // sh) + 1
-        nw = ((w - fw + (2 * pw)) // sw) + 1
-        A_prev = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
-    Z = np.zeros((m, nh, nw, c))
-    for i in range(nh):
-        h_start = i * sh
-        h_end = h_start + fh
-        for j in range(nw):
-            w_start = j * sw
-            w_end = w_start + fw
-            A_prev_slice = A_prev[:, h_start:h_end, w_start:w_end]
-            for k in range(c):
-                Z[:, i, j, k] = np.sum(A_prev_slice * W[:, :, :, k], axis=(1, 2, 3))
-    A = activation(Z + b)
-    return A
+        ph = int((h_prev * (sh - 1)  + kh - sh) / 2)
+        pw = int((w_prev * (sw - 1)  + kw - sw) / 2)
+
+    # out contains the output of convolution layer, careful may gives wrog out_h and out_w
+    out_h = int((h_prev + 2 * ph - kh)/ sh + 1)
+    out_w = int((w_prev + 2 * pw - kw)/ sw + 1)
+    out_c = c_new
+    conv = np.zeros((m,out_h,out_w,out_c))
+    # print("out", out_h, out_w, out_c)
+
+    # gives the padded input
+    A_prev_pad = np.pad(A_prev,((0, 0), (ph, ph),(pw, pw),(0,0)))
+    # print("a_prev_pad", A_prev_pad.shape)
+    # print("Weights", W.shape)
+
+
+
+    for i in range(out_h):
+        for j in range(out_w):
+            for c in range(out_c):
+                # crop A_prev_pad by filter, the sum, 
+
+                x = np.multiply(A_prev_pad[:,i:i+kh, j:j+kw,0:c_prev], W[: ,: ,0:c_prev,c])
+                # print("x before sum",x.shape)
+                x = np.sum(x, axis=(1,2,3)) 
+                # print("x after",x.shape)
+                x = activation(x+ b[0,0,0,c])
+                conv[:, i, j, c] = x
+                    
+    return conv
