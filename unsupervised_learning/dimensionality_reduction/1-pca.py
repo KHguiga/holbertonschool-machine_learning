904c732ac90ae1b2f2e276521289667723286d6b
khaled <khaled.gaguiga@holbertonschool.com>
2024-07-26 15:41:07 +0100

    pca 1

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def pca(X, ndim):
+    """
+    Takes X a matrix,
+    return Tr( reduction score)
+    """
+    n = X.shape[0]  # number of data points
+    d = X.shape[1]  # number of dimensions
+
+    # normalize X
+    X = X - np.mean(X, axis=0)
+
+    # decompose X into SVD
+    U, S, _ = np.linalg.svd(X)
+
+    # reduction dimension r
+    if ndim >= d:
+        ndim = d
+    Ur = U[:, 0:ndim]
+    Sr = np.diag(S[0:ndim])
+    T = Sr @ Ur
+    return T
