ae0599598f11f9c579439dfa1adef7675e722470
khaled <khaled.gaguiga@holbertonschool.com>
2024-07-31 17:27:06 +0100

    la t5

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 """
-This module contains the function add_matrices2D
+Task 5: 5. Across The Planes
 """
 
 
 def add_matrices2D(mat1, mat2):
-    """returns the elementwise addition of two 2D matrices"""
-    if len(mat1) != len(mat2) or not len(mat1) or len(mat1[0]) != len(mat2[0]):
-        return None
-    addition = []
+    """
+     Add two matrices element-wise.
+
+    Args:
+        mat1: the first matrice.
+        mat2: the second matrice.
+
+    Returns:
+        None: if mat1 and mat2 are not the same shape.
+        SommedArr: the new matrice with sommed values.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return (None)
+    SommedMat = []
+    # if len(mat1[0]) == 0:
+    #     SommedMat.append([])
+    #     return SommedMat
     for i in range(len(mat1)):
-        addition.append([])
-        for j in range(len(mat1[i])):
-            addition[i].append(mat1[i][j] + mat2[i][j])
-    return addition
+        SommedMat.append([])
+        # if len(mat1[0]) == 0:
+        #     break
+        for j in range(len(mat1[0])):
+            SommedMat[i].append(mat1[i][j] + mat2[i][j])
+    return SommedMat
