d82b0bb8bb553150b9fc988a45b55f2c270e94af
khaled <khaled.gaguiga@holbertonschool.com>
2024-11-11 19:58:17 +0100

    t2 malek

--- reinforcement_learning/temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -17,12 +17,9 @@ def td_lambtha(env, V, policy, lambtha,
             action = policy(s)
             s_new, reward, terminated, truncated, _ = env.step(action)
             delta = reward + (gamma * V[s_new]) - V[s]
-            E[s] += 1
-
             E *= gamma * lambtha
+            E[s] += 1
             V = V + alpha * delta * E
-
-            
             if terminated or truncated:
                 break
             s = s_new
