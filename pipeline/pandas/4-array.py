94884bca0db9b2f4bf15755637a1aecc49ed3cc4
khaled <khaled.gaguiga@holbertonschool.com>
2024-11-19 14:45:27 +0100

    pd 4

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def array(df):
+    # Select the last 10 rows of 'High' and 'Close' columns
+    last_10_rows = df[['High', 'Close']].tail(10)
+    
+    # Convert the selected rows into a NumPy array
+    return last_10_rows.to_numpy()
