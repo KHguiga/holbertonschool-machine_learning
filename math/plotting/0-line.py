99fbbac8b8b9be40d283f5423163aef6b643fbe7
khaled <khaled.gaguiga@holbertonschool.com>
2024-12-09 17:12:15 +0100

    plotting t0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
-import matplotlib.pyplot as plt
+"""
+line.py
+"""
+
 import numpy as np
+import matplotlib.pyplot as plt
 
-mean = [69, 0]
-cov = [[15, 8], [8, 15]]
-np.random.seed(5)
-x, y = np.random.multivariate_normal(mean, cov, 2000).T
-y += 180
 
-plt.plot(x, y, 'm.')
-# OR plt.scatter(x, y, c='m', marker='.')
-plt.xlabel("Height (in)")
-plt.ylabel("Weight (lbs)")
-plt.title("Men's Height vs Weight")
-plt.show()
+def line():
+    """
+    line -- plotting Y as a line graph
+    
+    Return: None
+    """
+    
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, color="red")
+    plt.show()
