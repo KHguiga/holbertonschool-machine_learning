fa78078baa9d6ec96ae31a07ab160bbcb36ca4f3
KHguiga <143078881+KHguiga@users.noreply.github.com>
2025-03-07 15:18:35 +0000

    Update 2-sarsa_lambtha.py change eps_greedy

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -18,7 +18,14 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
     n_states, n_actions = Q.shape
     max_epsilon = epsilon
     E = np.zeros((n_states, n_actions))
-
+    
+    def get_action(epsilon, Qs):
+        p = np.random.uniform()
+        if p > epsilon:
+            return np.argmax(Qs)
+        else:
+            return np.random.randint(0, 4)
+          
     for episode in range(episodes):
         E.fill(0)
         state = env.reset()[0]
@@ -53,10 +60,3 @@ def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100, alpha=0.1,
         epsilon = min_epsilon + (max_epsilon - min_epsilon) \
             * np.exp(-epsilon_decay * episode)
     return Q
-
-
-def get_action(state, Q, epsilon):
-    """Choose action using epsilon-greedy policy"""
-    if np.random.uniform(0, 1) <= epsilon:
-        return np.random.randint(0, Q.shape[1])
-    return np.argmax(Q[state, :])
